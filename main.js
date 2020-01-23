40c33b6b0cca707219fb709656ce429c3924040a
leoniekooijman <leoniekooijman@gmail.com>
2020-01-23 12:23:05 +0100

    changed it to an array

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-var code = "0000";
-function addCoin(){
-    for (x = 1; x < code.length + 1; x++){
-        if (code.charAt(x-1) == "0"){
-            code = vervang(x);
-        
-            if (code.charAt(0) == "1"){
-        var jojo = document.getElementsByTagName("td");
-        jojo = Array.prototype.slice.call(jojo);
-        jojo.reverse();
-        jojo[x-1].style.backgroundColor = "blue";
-            }
-            break;
-          
-        }
-    }
+var code = ["0","0","0","0"];
     
+function addCoin(){
+    for(var x = 0; x < code.length ; x++){
+        if(code[x] == "0"){
+            code[x] = "1";
+
+            var muntje = document.getElementsByTagName("td");
+            muntje = Array.prototype.slice.call(muntje);
+            muntje.reverse();
+            muntje[x].style.backgroundColor = "blue";
+            break;
+        }
+        
+          
+    }
+
     console.log(code);
-
 }
 
-function vervang(index){
-var eersteStuk = code.substr(0,index-1);
-var tweedeStuk = code.substr(index);
-return eersteStuk + "1" + tweedeStuk;
-}
+
