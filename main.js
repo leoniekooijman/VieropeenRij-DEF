06bf8e0cf96d09ca762efb06b4ad99aa2cdaef66
leoniekooijman <leoniekooijman@gmail.com>
2020-01-24 17:35:10 +0100

    1 rij te klikken aan de bovenkant

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-var code = ["0","0","0","0"];
+var speler = 1;
+var spelBord = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
     
-function addCoin(){
-    for(var x = 0; x < code.length ; x++){
-        if(code[x] == "0"){
-            code[x] = "1";
+function addCoin(col){
+  for(var row = 0; row < 6; row++){
+      for (var col = 0; col < 7; col++){
+        if(spelBord[row][col] == 0){
+            spelBord[row][col] = 1;
+
 
             var muntje = document.getElementsByTagName("td");
             muntje = Array.prototype.slice.call(muntje);
-            muntje.reverse();
-            muntje[x].style.backgroundColor = "blue";
+            muntje[col].style.backgroundColor = "blue";
             break;
-        }
-        
-          
+        } 
     }
 
-    console.log(code);
 }
-
-
+        
+    console.log(spelBord);
+    
+}
